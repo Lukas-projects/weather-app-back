bf3191fceb279460fb3abec1a801d7f6cba935fa
LukasLapienis <lapienislukasp@gmail.com>
2024-04-07 00:52:13 +0300

    update

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const axios = require('axios');
 const app = express();
 
-app.get('/weather-app', (req, res) => {
-  res.json({ users: ['userOne', 'userTwo', 'userThree', 'userFour'] });
+const weatherUrl = 'https://api.meteo.lt/v1/places/vilnius/forecasts/long-term';
+
+app.get('/weather', async (req, res) => {
+  try {
+    const response = await axios.get(weatherUrl);
+    const weatherData = response.data;
+    res.json(weatherData);
+  } catch (error) {
+    res.status(500).json({ error: 'error fetching weather data.' });
+  }
 });
 
 app.listen(5000, () => {
